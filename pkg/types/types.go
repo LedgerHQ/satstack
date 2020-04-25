d592fede9e997b516edadbbec444bf1090d82843
Anirudha Bose <anirudha.bose@ledger.fr>
2020-04-25 18:46:07 +0530

    Fix bug with missing output index when value is 0

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -11,7 +11,7 @@ type UTXO struct {
 type Input struct {
 	Coinbase    string   `json:"coinbase,omitempty"`         // [coinbase] The coinbase encoded as hex
 	OutputHash  string   `json:"output_hash,omitempty"`      // [non-coinbase] Same as transaction ID of vin
-	OutputIndex uint32   `json:"output_index,omitempty"`     // [non-coinbase] Index of the corresponding UTXO
+	OutputIndex uint32   `json:"output_index"`               // [non-coinbase] Index of the corresponding UTXO
 	Value       int64    `json:"value,omitempty"`            // [non-coinbase] Value of the corresponding UTXO in satoshis
 	Address     string   `json:"address,omitempty"`          // [non-coinbase] Address of the corresponding UTXO; can be empty
 	ScriptSig   string   `json:"script_signature,omitempty"` // [non-coinbase] Hex-encoded signature script
