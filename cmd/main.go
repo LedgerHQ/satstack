e395cafc6fd8ef7e1a693e98b6a05973460a34bc
Anirudha Bose <anirudha.bose@ledger.fr>
2020-06-03 00:10:56 +0530

    Load and import account xpubs from config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
+	"path"
 
 	"ledger-sats-stack/pkg/httpd"
+	"ledger-sats-stack/pkg/types"
 
+	"github.com/mitchellh/go-homedir"
 	log "github.com/sirupsen/logrus"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 	bolt "go.etcd.io/bbolt"
@@ -32,6 +36,46 @@ func main() {
 	}
 	defer db.Close()
 
+	accounts := loadAccountsConfig()
+	_ = xrpc.ImportAccounts(accounts)
+
 	engine := httpd.GetRouter(xrpc, db)
 	engine.Run(":20000")
+}
+
+func loadAccountsConfig() []types.Account {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Cannot obtain user home directory")
+	}
+
+	configPath := path.Join(home, ".sats.json")
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Cannot open config file")
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	accounts := []types.Account{}
+
+	err = decoder.Decode(&accounts)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Fatal("Cannot decode accounts config JSON")
+	}
+
+	log.WithFields(log.Fields{
+		"path":        configPath,
+		"numAccounts": len(accounts),
+	}).Info("Loaded config file")
+
+	return accounts
 }
