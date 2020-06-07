75e2cf6689c6b086bde9080e243e2a4d427992fc
Anirudha Bose <anirudha.bose@ledger.fr>
2020-06-07 22:40:29 +0530

    Refresh block verification progress every 5 seconds

--- pkg/httpd/xrpc.go
+++ pkg/httpd/xrpc.go
@@ -111,10 +111,10 @@ func WaitForNodeSync(xrpc transport.XRPC) {
 		}
 
 		log.WithFields(log.Fields{
-			"progress": fmt.Sprintf("%.2f %%", info.VerificationProgress*100),
+			"progress": fmt.Sprintf("%f %%", info.VerificationProgress*100),
 		}).Info("Sychronizing")
 
-		time.Sleep(2 * time.Second)
+		time.Sleep(5 * time.Second)
 	}
 }
 
