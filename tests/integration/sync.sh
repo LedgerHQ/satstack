133031ac71daabf37822832e1d7dfb9cf601939e
Anirudha Bose <anirudha.bose@ledger.fr>
2020-06-14 03:33:53 +0530

    Refactor sync benchmark script to perform cached sync as well

--- tests/integration/sync.sh
+++ tests/integration/sync.sh
@@ -12,25 +12,39 @@ do
     scheme=$(echo "$account" | cut -d"," -f2)
 
     echo -e ""
-    echo -e "${CYAN}SCHEME:${NC} ${scheme}"
-    echo -e "${CYAN}XPUB:${NC}   ${xpub}"
+    echo -e "${CYAN}SCHEME:${NC}         ${scheme}"
+    echo -e "${CYAN}XPUB:${NC}           ${xpub}"
 
     cmd="time ledger-live sync --xpub $xpub -c bitcoin_testnet -s ${scheme/standard/} -f summary"
 
     ledger-live libcoreReset
-    echo -e "${CYAN}SYNC:${NC}   explorers.api.live.ledger.com"
-    want=$($cmd)
+    echo -e "${CYAN}SYNC NOCACHE:${NC}   explorers.api.live.ledger.com"
+    wantnc=$($cmd)
+    echo -e "${CYAN}SYNC CACHE:${NC}     explorers.api.live.ledger.com"
+    _=$($cmd)  # only useful for benchmarks
 
     ledger-live libcoreReset
-    echo -e "${CYAN}SYNC:${NC}   0.0.0.0:20000"
-    got=$(EXPERIMENTAL_EXPLORERS=1 EXPLORER="http://0.0.0.0:20000" $cmd)
+    echo -e "${CYAN}SYNC NOCACHE:${NC}   0.0.0.0:20000"
+    gotnc=$(EXPERIMENTAL_EXPLORERS=1 EXPLORER="http://0.0.0.0:20000" $cmd)
 
-    if [ "$got" = "$want" ]; then
-      echo -e "${CYAN}OUTPUT:${NC} $got"
+    echo -e "${CYAN}SYNC CACHE:${NC}     0.0.0.0:20000"
+    gotc=$(EXPERIMENTAL_EXPLORERS=1 EXPLORER="http://0.0.0.0:20000" $cmd)
+
+    if [ "$gotnc" = "$wantnc" ]; then
+      echo -e "${CYAN}OUTPUT:${NC}         $gotnc"
       echo -e "${GREEN}Synchronization OK ✅${NC}"
     else
-        echo -e "${RED}WANT:${NC}    $want"
-        echo -e "${RED}GOT:${NC}     $got"
+        echo -e "${RED}WANT:${NC}           $wantnc"
+        echo -e "${RED}GOT:${NC}            $gotnc"
+        echo -e "${RED}Unexpected response ❌${NC}"
+        exit 1
+    fi
+
+    if [ "$gotnc" = "$gotc" ]; then
+      echo -e "${GREEN}Cached synchronization OK ✅${NC}"
+    else
+        echo -e "${RED}WANT:${NC}           $gotnc"
+        echo -e "${RED}GOT:${NC}            $gotc"
         echo -e "${RED}Unexpected response ❌${NC}"
         exit 1
     fi
