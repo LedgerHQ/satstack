e0fe66313b4adfae70a9dcad179559f352f0d509
Anirudha Bose <anirudha.bose@ledger.fr>
2020-06-20 14:16:29 +0530

    Fix fees integration tests

--- tests/integration/fees_test.go
+++ tests/integration/fees_test.go
@@ -2,7 +2,9 @@ package integration
 
 import (
 	"fmt"
+	"ledger-sats-stack/bus"
 	"ledger-sats-stack/httpd"
+	"ledger-sats-stack/httpd/svc"
 	utils "ledger-sats-stack/tests"
 	"net/http/httptest"
 	"os"
@@ -11,17 +13,27 @@ import (
 )
 
 func TestFeesIntegration(t *testing.T) {
-	// Setup phase
-	xrpc := httpd.GetBus(
+	b, err := bus.New(
 		os.Getenv("BITCOIND_RPC_HOST"),
 		os.Getenv("BITCOIND_RPC_USER"),
 		os.Getenv("BITCOIND_RPC_PASSWORD"),
 		os.Getenv("BITCOIND_RPC_ENABLE_TLS") == "true",
 	)
-	// Inject Gin router into an HTTP server
-	ts := httptest.NewServer(httpd.GetRouter(xrpc))
+	if err != nil {
+		t.Fatalf("Failed to initialize Bus: %v", err)
+	}
+	defer b.Close()
 
-	endpoint := fmt.Sprintf("%s/blockchain/v3/%s/fees", ts.URL, xrpc.Currency)
+	s := &svc.Service{
+		Bus: b,
+	}
+
+	// Inject Gin router into an HTTP server
+	engine := httpd.GetRouter(s)
+	ts := httptest.NewServer(engine)
+	defer ts.Close()
+
+	endpoint := fmt.Sprintf("%s/blockchain/v3/%s/fees", ts.URL, b.Currency)
 	responseBytes, err := utils.GetResponseBytes(endpoint)
 	if err != nil {
 		t.Fatalf("Expected no error, got %v", err)
@@ -39,8 +51,4 @@ func TestFeesIntegration(t *testing.T) {
 	if fees["last_updated"].(float64) > float64(time.Now().Unix()) {
 		t.Fatalf("last_updated value is greater than current timestamp: %v", fees)
 	}
-
-	// Teardown phase
-	xrpc.Shutdown()
-	ts.Close()
 }
