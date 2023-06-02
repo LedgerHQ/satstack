603ede9f7ac851a244407fc3dc721eda7c231244
Adrien Lacombe <adrien.lacombe@ledger.fr>
2023-06-02 08:11:27 +0200

    bump satstack version to 0.19.0

--- version/version.go
+++ version/version.go
@@ -9,7 +9,7 @@ import (
 var GitCommit string
 
 // Version returns the main version number that is being run at the moment.
-const Version = "v0.18.0"
+const Version = "v0.19.0"
 
 // Build indicates whether the build was a development or a production build.
 var Build string
